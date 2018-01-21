6281b356e7b276ff14faf341e807a4705aef0f58
Guedes <paulo.guedes@vistajet.com>
2018-01-21 18:02:45 -0200

    cart items quantity on navBar

--- src/containers/NavBar/index.js
+++ src/containers/NavBar/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Link  } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './style.css';
 
-const NavBar = (props) => (
-  <ul>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/top-5">Top-5 Products</Link></li>
-    <li><Link to="/cart">Your Cart</Link></li>
-  </ul>
-);
+const NavBar = (props) =>  {
+  const cartItemsQuantity = props.cart.ids.length;
 
-export default NavBar;
+  return (
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/top-5">Top-5 Products</Link></li>
+      <li className="quantity-line">
+        <Link to="/cart">Your Cart</Link>
+        {cartItemsQuantity ?
+          <div className="cart-quantity" >{cartItemsQuantity}</div>
+          :
+          null
+        }
+      </li>
+    </ul>
+  );
+};
+
+export default connect(({ cart }) => ({ cart }))(NavBar);
