1eebfc1761323a229d958e78f3935f2f3530d417
Paulo Guedes <paulogc86@hotmail.com>
2018-03-18 18:00:08 -0300

    nav bar

--- src/containers/Nav.jsx
+++ src/containers/Nav.jsx
@@ -3,29 +3,37 @@ import { Link  } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 
-const CustomNav = ({ ids }) => (
-  <Navbar>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <Link to="/cousines">Home</Link>
-      </Navbar.Brand>
-    </Navbar.Header>
-    <Nav key="cart-lin">
-      <Navbar.Brand>
-        <Link to="/cart">Your Cart</Link>
-      </Navbar.Brand>
-    </Nav>
-    <Nav key="cart-items">
-      <Navbar.Brand>
-        Items in your cart: {ids.length}
-      </Navbar.Brand>
-    </Nav>
-    <Nav pullRight>
-      <Navbar.Brand>
-        <Link to="/">Logout</Link>
-      </Navbar.Brand>
-    </Nav>
-  </Navbar>
-);
+const CustomNav = ({ ids, authenticated }) => {
+  if (!authenticated) {
+    return null;
+  }
 
-export default connect(({ cartReducer }) => ({ ...cartReducer }))(CustomNav);
+  return (
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/cousines">Home</Link>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav key="cart-lin">
+        <Navbar.Brand>
+          <Link to="/cart">Your Cart</Link>
+        </Navbar.Brand>
+      </Nav>
+      <Nav key="cart-items">
+        <Navbar.Brand>
+          Items in your cart: {ids.length}
+        </Navbar.Brand>
+      </Nav>
+      <Nav pullRight>
+        <Navbar.Brand>
+          <Link to="/">Logout</Link>
+        </Navbar.Brand>
+      </Nav>
+    </Navbar>
+  );
+};
+
+export default connect(
+  ({ cartReducer, authReducer }) => ({ ...cartReducer, ...authReducer }))
+(CustomNav);
